b7882dc3b03cacb48c3b697cd0b6305537edb7b7
Thiago Pinho <thiagopinho05@gmail.com>
2025-07-18 20:56:45 -0300

    chore: changing application name widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,16 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:skillforge/pages/home_screen.dart';
 import 'package:skillforge/pages/login/login_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(const MyApp());
+  runApp(const SkillForgeApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class SkillForgeApp extends StatelessWidget {
+  const SkillForgeApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
