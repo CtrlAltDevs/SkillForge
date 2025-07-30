ae01b37df93e0b7d49005b089d55585b8b33d2c3
Thiago Pinho <thiagopinho05@gmail.com>
2025-07-30 20:45:56 -0300

    feat: implementing multi bloc provider and temporary instance of authRepository

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,34 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:skillforge/authentication/bloc/auth_bloc.dart';
+import 'package:skillforge/authentication/repository/auth_repository.dart';
 import 'package:skillforge/core/routes.dart';
 import 'package:skillforge/utils/app_theme.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(const SkillForgeApp());
+  runApp(SkillForgeApp());
 }
 
 class SkillForgeApp extends StatelessWidget {
-  const SkillForgeApp({super.key});
+  final AuthRepository _authRepository = AuthRepository();
+  SkillForgeApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      routerConfig: routes,
-      title: 'Flutter Demo',
-      theme: AppTheme.lightTheme(),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<AuthBloc>(
+          create: (_) => AuthBloc(authRepository: _authRepository),
+        ),
+      ],
+      child: MaterialApp.router(
+        routerConfig: routes,
+        title: 'Flutter Demo',
+        theme: AppTheme.lightTheme(),
+      ),
     );
   }
 }
